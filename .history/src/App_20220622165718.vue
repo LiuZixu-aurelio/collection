cf4d578d62a378d752514c84394e124200bbc393
Liu Zixu <398488080@qq.com>
2022-06-22 16:58:43 +0800

    up

---
+++ .history/src/App_20220622165718.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="title">设计导航</div>
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+.title {
+  background-color: #000;
+  color: #fff;
+  justify-content: center;
+  align-items: center;
+  position: ;
+  height: 36px;
+}
+body {
+  margin: 0px;
+}
+</style>
